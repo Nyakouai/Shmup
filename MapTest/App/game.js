d1788054a6008999158d49b61db1e93c0f106481
Julien Bruyère <julien.bruyere.1991@gmail.com>
2016-01-27 17:42:48 +0100

    Abandon conversion TileMap -> TileSprite Mise en place du json de base

--- MapTest/App/game.js
+++ MapTest/App/game.js
@@ -19,18 +19,22 @@
  * @param {Game} game accessor of the Phaser's game object
  */
 BasicGame.Game = function (game) {
-	var level;
-	var player;
-	var enemies;
-
-	var powerupItems;
-	var bombItems;
-	
-	var score;
-	var scoreText;
-	var playerLifeText;
-	var bombText;
+	var map;
 };
+
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'App/Assets/tileset.json', false); 
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+}
 
 BasicGame.Game.prototype = {
 
@@ -42,58 +46,28 @@ BasicGame.Game.prototype = {
   		this.game.renderer.renderSession.roundPixels = true;
 		this.physics.startSystem(Phaser.Physics.ARCADE);
 
-		var pause = this.game.input.keyboard.addKey(Phaser.Keyboard.P);
-		pause.onDown.add(function () {
-			game.paused = true;
-			level.autoScroll(0, 0);
+		var tileSize = 32;
+		var width = 25, height = 60;
 
-			var grayfilter = this.game.add.filter('Gray');
+		map = this.game.add.tilemap(null, tileSize, tileSize, width, height);
+		map.addTilesetImage('tileset', null, tileSize, tileSize, 0, 1);
 
-			this.world.filters = [grayfilter];
-			
-			this.loadingText = this.add.text(this.game.width / 2, 100, "Press P or tap/click game to resume", { font: "20px monospace", fill: "#fff" });
-	    	this.loadingText.anchor.setTo(0.5, 0.5);
+		layer = map.create('layer1', width, height, tileSize, tileSize);
 
-	    	this.saveButton = this.game.add.sprite(this.game.width / 2, this.game.height / 2 - 50, 'button');
-	    	this.saveButton.anchor.setTo(0.5, 0.5);
-	    	this.saveText = this.add.text(this.game.width / 2, this.game.height / 2 - 50, "Sauver", { font: "25px monospace", fill: "#000" });
-	    	this.saveText.anchor.setTo(0.5, 0.5);
+		var jsonObj;
+		loadJSON(function(response) {
+	  		jsonObj = JSON.parse(response);
+ 		});
 
-	    	this.loadButton = this.game.add.sprite(this.game.width / 2, this.game.height / 2 + 50, 'button');
-	    	this.loadButton.anchor.setTo(0.5, 0.5);
-	    	this.loadText = this.add.text(this.game.width / 2, this.game.height / 2 + 50, "Charger", { font: "25px monospace", fill: "#000" });
-	    	this.loadText.anchor.setTo(0.5, 0.5);
-    	});
+ 		console.log(jsonObj);
 
-		var unpause = this.game.input.keyboard.addKey(Phaser.Keyboard.M);
-		unpause.onDown.add(this.unpauseGame, this);
-		
-		level = new Level(this.game);
 
-		player = new Player(this.game);
+		for(var j=0; j<height; j++)
+			for(var i=0; i<width; i++)
+				map.putTile(8, i, j, layer);
 
-		enemies = [];
-		enemies.push(new Enemies.Enemy1(this.game));
-		enemies.push(new Enemies.Enemy2(this.game));
-		enemies.push(new Enemies.Enemy3(this.game));
-		enemies.push(new Enemies.EnemyPowerup(this.game));
-		enemies.push(new Enemies.EnemyBomb(this.game));
-		enemies.push(new Enemies.EnemyTowerBoss(this.game));
-		enemies.push(new Enemies.Boss1(this.game));
-		level.enemies = enemies;
-    
-    	powerupItems = new Collectible.Powerup(this.game);
-    	powerupItems.enableBody = true;
-    	bombItems = new Collectible.Bomb(this.game);
-    	bombItems.enableBody = true;
-		
-		score=0;
-		scoreText = this.add.text(10, 600-40, 'Score: 0', 
-			{ fontSize: '32px', fill: '#000' });
-		playerLifeText = this.add.text(800-100, 600-80, 'Life: 3', 
-			{ fontSize: '32px', fill: '#000' });
-		bombText = this.add.text(800-150, 600-40, 'Bombs: 3', 
-			{ fontSize: '32px', fill: '#000' });
+			map.putTile(6, 1, 2, layer);
+
   	},
 
 	/**
@@ -102,81 +76,6 @@ BasicGame.Game.prototype = {
 	 */
 	update: function () {
 
-		this.physics.arcade.overlap(enemies, player.weapons, this.bulletcollisionHandler, null, this)
-		this.physics.arcade.overlap(player, enemies, this.playerCollisionHandler, null, this)
-		
-    	this.physics.arcade.overlap(player, powerupItems, this.playerPowerup, null, this);
-    	this.physics.arcade.overlap(player, bombItems, this.playerBomb, null, this);
-	
-		for(var i=0; i<enemies.length; i++){
-			enemies[i].forEach(function (enemy){
-				this.physics.arcade.overlap(player, enemy.weapons, this.bulletEnemyCollisionHandler, null, this)
-			},this);
-		}
-	},
-
-	/**
-   	 * Handle collisions between a bullet and an ennemy
-   	 * @param  {Bullet} bullet The bullet to test 
-   	 * @param  {Ennemy} ennemy The ennemy to test
-   	 */
-	bulletcollisionHandler: function (enemy, bullet) {
-		if(!bullet.indestructible){
-			bullet.kill();
-		}
-		
-		enemy.damage(bullet.power);
-	
-		if(enemy.alive){
-			//enemy.play('hit');
-			this.time.events.repeat(50,2,blink,this,enemy);
-		}
-		else{
-			if(enemy.powerup){
-				powerupItems.appear(enemy);
-			}
-			if(enemy.bomb){
-				bombItems.appear(enemy);
-			}
-			enemy.explode();
-			enemy.kill();
-			score += enemy.score;
-		}
-		
-		scoreText.text = 'Score: ' + score;
-	},
-	
-
-	playerCollisionHandler: function (player, enemy) {
-		if(!player.cooldown && !player.appearing){
-			player.takeDamage();
-			//ennemy.kill();
-			playerLifeText.text = 'Life: ' + player.life;
-		}
-	},
-
-	bulletEnemyCollisionHandler: function(player, bullet){
-		if(!player.cooldown && !player.appearing){
-			player.takeDamage();
-			bullet.kill();
-			playerLifeText.text = 'Life: ' + player.life;
-		}
-
-	},
-
-	playerPowerup: function(player, powerup) {
-    	powerup.kill();
-    	if(player.weaponLevel < 2){
-        	player.weaponLevel++;
-    	}
-    	else{
-    		score += 50;
-    	}
-	},
-
-	playerBomb: function(player, bomb) {
-    	bomb.kill();
-    	player.countBombs++;
 	},
 
 	/**
@@ -186,74 +85,6 @@ BasicGame.Game.prototype = {
 	render: function () {
 
 		this.game.debug.cameraInfo(this.camera, 32, 32);
-		this.game.debug.text("Current time: " + this.game.time.time, 30, 120);
-		this.game.debug.text("Progress: " + level.progress, 30, 150);
-		
-		//Debug Hitbox
-		
-		//this.game.debug.body(player);
-		/*player.weapons[player.weaponLevel].forEach(function (bullet) {
-        	bullet.game.debug.body(bullet);
-    	});
-    
-    	player.weapons[3].forEach(function (bullet) {
-        	bullet.game.debug.body(bullet);
-    	});
-		
-    	enemies[3].forEach(function (enemy) {
-        	enemy.game.debug.body(enemy);
-        	for(var i=0; i<4; i++){
-        		enemy.weapons[i].forEach(function (bullet){
-        			bullet.game.debug.body(bullet);
-        		});
-        	}
-    	});
-  		*/
-	},
-
-	unpauseGame: function() {
-		if(this.game.paused)
-		{
-			if(event.x > this.saveButton.x && event.x < this.saveButton.x+this.saveButton.width 
-            	&& event.y > this.saveButton.y && event.y < this.saveButton.x+this.saveButton.height ){	
-                console.log("save");
-           	 	this.saveData();
-            }
-            else if(event.x > this.loadButton.x && event.x < this.loadButton.x+this.loadButton.width 
-            	&& event.y > this.loadButton.y && event.y < this.loadButton.x+this.loadButton.height ){
-                this.loadData();
-            }
-		}
-		else
-    	{
-			this.loadingText.destroy();
-			this.saveButton.destroy();
-			this.saveText.destroy();
-			this.loadButton.destroy();
-			this.loadText.destroy();
-
-			this.world.filters = null;
-			level.autoScroll(0, 50);
-			this.game.paused = false;
-		}
-	},
-
-	saveData: function() {
-		if(!store.enabled) {
-    		alert('Sauvegarde non supportée sur votre navigateur. Desactivez le mode privé ou changez de navigateur');
-    		return;
-    	}
-
-	    player.saveData();
-	},
-
-	loadData: function() {
-		if(!store.enabled) {
-    		alert('Sauvegarde non supportée sur votre navigateur. Desactivez le mode privé ou changez de navigateur');
-    		return;
-    	}
-
-	    player.loadData();
 	},
 
 	quitGame: function (pointer) {
